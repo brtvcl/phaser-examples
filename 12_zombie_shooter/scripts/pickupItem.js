ab6d98734411a15a8af93dfc15be0016acb62d8e
Berat Vıcıl <beratvicil@gmail.com>
2023-12-30 21:31:57 +0300

    add secondary weapon pickup logic

--- 12_zombie_shooter/scripts/pickupItem.js
+++ 12_zombie_shooter/scripts/pickupItem.js
@@ -1,7 +1,7 @@
 import Zombie from "../object/zombie";
 import Item from "../object/item";
 import distance_to_point from "../helper/distance_to_point";
-import { SECONDARY_WEAPONS_CONFIG, ammoTypeItemMap, PRIMARY_WEAPONS_CONFIG } from "../constants";
+import { SECONDARY_WEAPONS_CONFIG, ammoTypeItemMap, PRIMARY_WEAPONS_CONFIG, SECONDARY_WEAPONS } from "../constants";
 
 function pickupItem(object) {
 	object.scene.children.list.forEach((child) => {
@@ -23,7 +23,7 @@ function pickupItem(object) {
 
 			const currentAmmoItemType = ammoTypeItemMap[PRIMARY_WEAPONS_CONFIG[object.primaryWeapon]?.ammo];
 			const currentSecondaryAmmoType = ammoTypeItemMap[SECONDARY_WEAPONS_CONFIG[object.secondaryWeapon]?.ammo];
-			// Pickup weapons
+			// Pickup primary weapons
 			if (["M4", "AR", "AK"].includes(child.type) && distanceToItem < 32 && Phaser.Input.Keyboard.JustDown(object.input.interact)) {
 				if (object.primaryWeapon) {
 					// Drop weapon
@@ -42,6 +42,26 @@ function pickupItem(object) {
 				object.primaryWeapon = child.type;
 				child.destroy();
 
+			}
+
+			// Pickup secondary weapons
+			if (Object.keys(SECONDARY_WEAPONS).includes(child.type) && distanceToItem < 32 && Phaser.Input.Keyboard.JustDown(object.input.interact)) {
+				if (object.secondaryWeapon) {
+					// Drop weapon
+					new Item({ x: object.x, y: object.y + 32, type: object.secondaryWeapon }, object.scene);
+
+					// Drop ammo
+					const pickedUpAmmoType = ammoTypeItemMap[SECONDARY_WEAPONS_CONFIG[child.type]?.ammo];
+					if (currentSecondaryAmmoType !== pickedUpAmmoType && object.ammo > 0) {
+						const ammoType = ammoTypeItemMap[SECONDARY_WEAPONS_CONFIG[object.secondaryWeapon].ammo];
+						new Item({ x: object.x, y: object.y + 48, type: ammoType, meta: object.ammo }, object.scene);
+						object.ammo = 0;
+					}
+
+				}
+
+				object.secondaryWeapon = child.type;
+				child.destroy();
 			}
 
 			// Pickup Primary ammo
